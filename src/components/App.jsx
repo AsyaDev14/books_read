1898678784e708b194c739afa2153215975dfe45
AsyaDev <142207257+AsyaDev14@users.noreply.github.com>
2024-07-31 12:39:47 +0300

    initiall commit && register form

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import { Register } from "../pages/register/Register";
+import Login from "../pages/login/Login";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
